7b8e7b9f89daf25b56d3dad6039077cbb8e77765
Artem Kliaus <artem.kliaus@gmail.com>
2016-06-30 17:28:14 +0300

    dropdmenu

--- js/main.js
+++ js/main.js
@@ -14,9 +14,17 @@ $('.controls')
 .bind('click', function(){
 	currentPosition = ($(this).attr('id') =='rightCtrl')
 	? currentPosition+1 : currentPosition-1;
-	if (currentPosition > 2) currentPosition = 0;
+	if (currentPosition > numberOfSlides-1) currentPosition = 0;
 	if (currentPosition < 0) currentPosition = 2;
 	$('#slideInner').animate({
 		'marginLeft' : slideWidth*(-currentPosition)
 	});
+});
+
+$(function(){
+    $('AllCat').find('> li').hover(function(){
+        $(this).find('ul')
+        .removeClass('AllCat')
+        .stop(true, true).slideToggle('fast');
+    });
 });
